dcb75f4cb7802f9b8129fe5f7cbfb270e024f6fc
ThilakAswin <53222461+ThilakAswin@users.noreply.github.com>
2023-05-26 23:14:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function flames() {
     const name1 = document.getElementById("yourname").value.toLowerCase().replace(/\s/g, '');
     const name2 = document.getElementById("crushname").value.toLowerCase().replace(/\s/g, '');
+    
+    if(name1 === "" || name2 === "")
+    {
+        document.querySelector("h2").innerHTML = "Please fill the required fields";
+    }
+    
+    else{
   
     // Calculate the count of unique characters in both names
     const uniqueChars = new Set(name1 + name2);
@@ -23,4 +30,5 @@ function flames() {
     // Display the result
     // document.getElementById("result").textContent = result;
     document.querySelector("h2").innerHTML = result;
+    }
   }
